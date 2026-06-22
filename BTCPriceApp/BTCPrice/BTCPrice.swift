34d3e589f895ce60f413ab5607c9e6054b7fd0fe
n913239 <n913239@gmail.com>
2026-06-22 16:01:33 +0800

    Introduce BTCPriceItem to represent the domain model

--- BTCPriceApp/BTCPrice/BTCPrice.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BTCPrice.swift
-//  BTCPrice
-//
-//  Created by mike on 2026/6/22.
-//
-
-import Foundation
-
---
+++ BTCPriceApp/BTCPrice/Domain/BTCPrice.swift
@@ -0,0 +1,14 @@
+//
+//  BTCPrice.swift
+//  BTCPrice
+//
+//  Created by mike on 2026/6/22.
+//
+
+public struct BTCPriceItem {
+    public let price: Double
+    
+    public init(price: Double) {
+        self.price = price
+    }
+}
